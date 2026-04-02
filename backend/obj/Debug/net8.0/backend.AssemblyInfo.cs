0334bcbf6fb2c806552b409dc8d1a96973d81743
h2okisme57 <24520557@gm.uit.edu.vn>
2026-04-02 20:53:36 +0700

    temp before pull

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3fa0a6963175b4b64103c3336369aed6876046e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a4f8f76424a2f8da105a293d7517922b46bb33")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
